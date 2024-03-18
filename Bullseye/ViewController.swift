2189e992d566d533f564d748fcfb2b7af2bf0e45
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2024-03-19 00:06:11 +0500

    added some styling

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -13,10 +13,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        assignBackground()
         showAlert(self)
     }
 
+    
+    func assignBackground() {
+        let background = UIImage(named: "Bullseye bg image.jpeg")
+
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
 //  text label where the random number to guess is shown
     @IBOutlet var guessNumber: UILabel!
     
