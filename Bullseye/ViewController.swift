d5bad2dae976d044b70f6fdbeb4f76958f48a97b
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2024-03-18 20:02:20 +0500

    part 4 done will add comments later

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -14,19 +14,65 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showAlert(self)
+        scoreNumber.text = "0"
     }
 
-
+    @IBOutlet var guessNumber: UILabel!
+    
+    var guessNum = 0
+    
+    @IBOutlet var score: UILabel!
+    
+    var scoreNum = 0
+    var allScoreSum = -50
+    
+    @IBOutlet var scoreNumber: UILabel!
+    
+    @IBOutlet var roundNum: UILabel!
+    
+    var roundNumber = 0
+    
+    @IBAction func startOverButton(_ sender: Any) {
+        roundNumber = 0
+        allScoreSum = 0
+        displayResult()
+        guessingNumber()
+    }
+    
+    func guessingNumber() -> Int {
+        guessNum = Int.random(in: 1...100)
+        guessNumber.text = String(guessNum)
+        return guessNum
+    }
+    
+    func displayResult() {
+        score.text = String(scoreNum)
+        scoreNumber.text = String(allScoreSum)
+        roundNum.text = String(roundNumber)
+    }
+    
     @IBAction func showAlert(_ sender: Any) {
+        
+        roundNumber += 1
+        
+        scoreNum = 100 - abs(guessNum - currentValue)
+        allScoreSum += scoreNum
+        
+        
         let message = "The value of current slider is: \(currentValue)"
         
-        let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: "Your score is \(scoreNum)", message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        displayResult()
+        guessNum = guessingNumber()
+        
     }
 
     @IBAction func sliderMoved(_ slider: UISlider) {
